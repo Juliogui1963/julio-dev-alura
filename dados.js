b19e7530e79e8c1dd0f055bbfdc6a4cc1d9f0ff4
Juliogui1963 <juliofortuna1963@gmail.com>
2024-09-05 09:06:38 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descrição : "Rayssa Leal, a Fadinha, é um fenômeno do skate mundial. Com apenas [idade], ela já conquistou o mundo com suas manobras incríveis e seu carisma. Sua participação nas Olimpíadas de Tóquio 2020 marcou a história do skate e a inspirou milhões de jovens." ,
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+
+    {
+        titulo : "Rebeca Andrade",
+        descrição : "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com um talento inegável e uma determinação ímpar, ela conquistou o coração dos brasileiros e se tornou um verdadeiro símbolo de superação. Sua trajetória é marcada por conquistas históricas, como as medalhas de ouro olímpicas e mundiais, que a colocaram no topo do pódio e na história do esporte brasileiro. Rebeca é mais do que uma atleta, ela é uma inspiração para milhões de jovens, mostrando que com muito trabalho e dedicação é possível alcançar os sonhos mais altos",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+
+    {
+        titulo : "Beatriz Souza",
+        descrição : "Beatriz Souza é a mais nova estrela do judô brasileiro.Com sua força e técnica impecável, ela conquistou o ouro olímpico em Paris 2024, inspirando uma nova geração de judocas. Sua trajetória é marcada por dedicação e superação, e ela se tornou um símbolo do esporte brasileiro",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
